9b628396e7face60a9b7573f485261cf634b1d1f
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-11 22:08:00 +0100

    add search engine selection

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 (function() {
 
-  console.log('Find now!')
-  
-  // 1.
-  // get the value url parameter `find` on page load
-  // transition page to duckduckgo with the value as search
-  // 2.
-  // before first space is a shortcode for the website to be searched on
-  // ex: `d test` = search `test` on duckduckgo, `g.uk test` = search `test` on google.co.uk
+	// site keyword : site URL
+  var searchEngines = {
+    d: 'https://duckduckgo.com/?q=',
+    g: 'https://encrypted.google.com/search?q=',
+    y: 'https://www.youtube.com/results?search_query=',
+    dd: 'http://devdocs.io/#q='
+  }
 
-  // https://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript
   function getURLParameter(name) {
-    var param = new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)|| [null, '']
-    return decodeURIComponent((param)[1].replace(/\+/g, '%20')) || null;
+    var param = new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''];
+    return decodeURIComponent(param[1].replace(/\+/g, '%20')) || null;
   }
 
-  function findQuery(query) {
-    if(!query) return
-    window.open('https://duckduckgo.com/?q=' + query, '_self');
+  function decodeUserQuery(userQuery) {
+    if(!userQuery) { return }
+
+		var siteQuery,
+				regexBang = /^!/,
+				siteKeyword,
+				siteURL,
+				terms = userQuery.split(' ');
+
+    if(terms.length > 1 && new RegExp(regexBang).test(terms[0])) {
+			siteKeyword = terms[0].replace(regexBang, '');
+			// searchEngines.hasOwnProperty(siteKeyword) &&
+      siteURL = searchEngines[siteKeyword] || null;
+			siteQuery = terms.splice(1, terms.length).join(' ');
+    } else {
+			siteQuery = userQuery;
+		}
+
+		return {
+			siteURL: siteURL || null,
+			siteQuery: siteQuery
+		};
   }
 
-  findQuery(getURLParameter('q'))
-  
+	function openSearchResults(siteURL, siteQuery) {
+		if(!siteURL) {
+			siteURL = searchEngines.d;
+		}
+		window.open(siteURL + siteQuery, '_self');
+	}
+
+  // start
+	var userQuery = decodeUserQuery(getURLParameter('q'));
+	openSearchResults(userQuery.siteURL, userQuery.siteQuery);
+
+	console.log('userQuery', userQuery);
 })();
