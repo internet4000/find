f9c22b225c065d81798c483e2338925143d7524f
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-11-12 17:40:38 +0100

    add woolfram and google keep

--- main.js
+++ main.js
@@ -12,12 +12,15 @@
 		dr: 'https://drive.google.com/drive/search?q=',
 		e: 'http://rover.ebay.com/rover/1/711-53200-19255-0/1?icep_ff3=9&pub=5575347480&toolid=10001&campid=5338215070&icep_sellerId=&icep_ex_kw=&icep_sortBy=12&icep_catId=&icep_minPrice=&icep_maxPrice=&ipn=psmain&icep_vectorid=229466&kwid=902099&mtid=824&kw=lg&icep_uq=',
     g: 'https://encrypted.google.com/search?q=',
+		k: 'https://keep.google.com/?q=#search/text%3D',
+		l: 'https://www.linguee.com/search?query=',
 		lbc: 'https://www.leboncoin.fr/annonces/offres/ile_de_france/occasions/?th=1&q=',
 		lp: 'https://lpepfinder.com/#gsc.q=',
 		m: 'https://www.google.com/maps/search/',
 		osm: 'https://www.openstreetmap.org/search?query=',
 		tr: 'https://translate.google.com/#auto/#auto/',
 		w: 'https://en.wikipedia.org/w/index.php?search=',
+		wo: 'http://www.wolframalpha.com/input/?i=',
     y: 'https://www.youtube.com/results?search_query=',
   };
 
