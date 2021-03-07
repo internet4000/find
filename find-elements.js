b6b45d24e3235a0c5fb6acf4b64edd1a8359b8e8
hugur <hugu.r.p+hello@pm.me>
2021-03-07 13:27:29 +0100

    add animations for web component search

--- find-elements.js
+++ find-elements.js
@@ -1,3 +1,57 @@
+const i4kFindLogo = class extends HTMLElement {
+	r4Logo = `
+<svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" version="1.1" viewBox="0 0 338.66666 338.66669">
+	<g transform="translate(349.25 -.66666)">
+		<rect style="stroke-width:.26458;fill:#80e5ff" height="254" width="254" y="43" x="-306.92"/>
+		<g aria-label="F " transform="matrix(6.397 0 0 6.5266 7127.1 -1495.4)">
+			<path d="m-1155.4 248.75h17.168v3.3203h-13.203v8.5938h11.973v3.3203h-11.973v13.926h-3.9648z"/>
+		</g>
+		<flowRoot xml:space="preserve" style="letter-spacing:0px;font-size:40px;word-spacing:0px;font-family:sans-serif;line-height:1.25;fill:#000000" transform="scale(.26458)">
+			<flowRegion>
+				<rect y="-117.48" width="1040" x="-2040" height="760"/>
+			</flowRegion>
+			<flowPara/>
+		</flowRoot>
+		<rect style="stroke-width:.26458;fill:none" height="338.67" width="338.67" y=".66666" x="-349.25"/>
+		<g aria-label="! " transform="matrix(6.397 0 0 6.5266 7235.5 -1495.4)">
+			<path d="m-1149.9 248.75h3.9649v12.793l-0.4102 6.9726h-3.1445l-0.4102-6.9726zm0 24.199h3.9649v4.9609h-3.9649z"/>
+		</g>
+	</g>
+</svg>
+	`
+	connectedCallback() {
+		this.render()
+	}
+	render() {
+		const $homeLink = document.createElement('a')
+		$homeLink.classList.add('Title')
+		$homeLink.title = "Find!"
+		$homeLink.innerHTML = this.r4Logo
+		this.append($homeLink)
+	}
+}
+
+
+const i4kFindApp = class extends HTMLElement {
+	connectedCallback() {
+		this.render()
+		this.querySelector('i4k-find').addEventListener('findSearch', (event) => {
+			this.setAttribute('searched', true)
+		})
+	}
+	render() {
+		this.innerHTML = `
+			<section class="App-header">
+				<i4k-find-logo></i4k-find-logo>
+				<i4k-find></i4k-find>
+			</section>
+
+			<section class="App-body">
+				<i4k-find-info></i4k-find-info>
+			</section>`
+	}
+}
+
 const i4kFind = class extends HTMLElement {
 	connectedCallback() {
 		this.render()
@@ -5,6 +59,11 @@ const i4kFind = class extends HTMLElement {
 	findSearch = (query) => {
 		if(!query) return false
 		Find.find(query)
+		const event = new CustomEvent('findSearch', {
+			bubbles: true,
+			detail: query
+		})
+		this.dispatchEvent(event)
 	}
 	handleSubmit = (event) => {
 		this.findSearch(this.search)
@@ -76,7 +135,6 @@ const i4kFindInfo = class extends HTMLElement {
 
 			symbolFns && symbolFns.forEach(fn => {
 				const $symbolInfoListItem = document.createElement('li')
-				console.log(symbolData)
 				$symbolInfoListItem.innerText = `${fn} : ${symbolData.fns[fn]}`
 				$symbolInfoList.append($symbolInfoListItem)
 			})
@@ -106,3 +164,5 @@ const i4kFindInfo = class extends HTMLElement {
 
 customElements.define('i4k-find', i4kFind)
 customElements.define('i4k-find-info', i4kFindInfo)
+customElements.define('i4k-find-logo', i4kFindLogo)
+customElements.define('i4k-find-app', i4kFindApp)
